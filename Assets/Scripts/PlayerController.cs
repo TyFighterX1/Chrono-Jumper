df110f54c3e4e666f26c08d76898aefd6785098f
TyFighterX1 <Tyler.Beck@me.com>
2025-04-09 01:55:51 -0400

    Jump Fine tuned

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,14 @@ public class PlayerController : MonoBehaviour
     public GameObject failFX;
     public Transform playerpos;
     public GameManager gm;
-    public CapsuleCollider2D hitBox;
-    public Vector2 standingBox;
-    public Vector2 crouchingBox;
+    //public CapsuleCollider2D hitBox;
+    //public Vector2 standingBox;
+    //public Vector2 crouchingBox;
     public Animator anim;
 
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 12f;
     private bool isFacingRight = true;
     
     [SerializeField]private Rigidbody2D rb;
@@ -23,9 +23,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        hitBox = GetComponent<CapsuleCollider2D>();
+        //hitBox = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
-        standingBox = hitBox.size;
+        //standingBox = hitBox.size;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -43,15 +43,17 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        if(Input.GetKeyDown(KeyCode.S)) // crouching
-        {
-            hitBox.size = crouchingBox;
-        }
+        //if(Input.GetKeyDown(KeyCode.S)) // crouching
+        //{
+        //    hitBox.size = crouchingBox;
+        //    anim.SetBool("IsCrouching", true);
+        //}
 
-        if(Input.GetKeyUp(KeyCode.S)) // not crouching
-        {
-            hitBox.size = standingBox;
-        }
+        //if(Input.GetKeyUp(KeyCode.S)) // not crouching
+        //{
+        //    hitBox.size = standingBox;
+        //    anim.SetBool("IsCrouching", false);
+        //}
 
         FlipPlayer();
     }
